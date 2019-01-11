bf8914c9d560bd2afca5aceeeceba97b570bd04c
Muosvr <lujason2015@gmail.com>
2019-01-11 09:49:26 +0800

    numbers

--- drawBoard.py
+++ drawBoard.py
@@ -1,9 +1,14 @@
-def drawBoard():
-    print("   |   |")
-    for _ in range(2):
+#!/bin/python3
+
+
+def drawBoard(arr):
+    print(f" {arr[0]} | {arr[1]} | {arr[2]}")
+    for i in range(1, 3):
         print("---+---+---")
-        print("   |   |")
-    
+        print(f" {arr[3 * i + 0]} | {arr[3 * i + 1]} | {arr[3 * i + 2]}")
+
 
 if __name__ == "__main__":
-    drawBoard()
+    import sys
+    print(sys.version)
+    drawBoard([0, 1, 2, 3, 4, 5, 6, 7, 8])
